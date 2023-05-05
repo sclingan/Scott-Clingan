b775edad86f0e5abcf0fef04e3788db7e01a3be4
Scott Clingan <sclingan1@gmail.com>
2023-05-05 04:11:58 -0500

    Added basic mobile layout

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,5 +1,10 @@
 import placeholder from '../public/image-colton.jpg'
-
+import social from '../public/social-proof.png'
+import article from '../public/article.png'
+import intro from '../public/intro.png'
+import base from '../public/base.png'
+import results from '../public/results.png'
+import faq from '../public/FAQ.png'
 
 function Projects() {
 
@@ -11,7 +16,7 @@ function Projects() {
         </div>
         <ul className="projects">
           <li>
-            <img src={placeholder}></img>
+            <img src={social}></img>
             <h4>Social Proof Section</h4>
             <p>HTML</p>
             <p>CSS</p>
@@ -19,7 +24,7 @@ function Projects() {
             <a href='' className='code-link'>View code</a>
           </li>
           <li>
-            <img src={placeholder}></img>
+            <img src={article}></img>
             <h4>Article Preview</h4>
             <p>HTML</p>
             <p>CSS</p>
@@ -27,7 +32,7 @@ function Projects() {
             <a href='' className='code-link'>View code</a>
           </li>
           <li> 
-            <img src={placeholder}></img>
+            <img src={intro}></img>
             <h4>Intro Component</h4>
             <p>HTML</p>
             <p>CSS</p>
@@ -35,7 +40,7 @@ function Projects() {
             <a href='' className='code-link'>View code</a>
           </li>
           <li>
-            <img src={placeholder}></img>
+            <img src={base}></img>
             <h4>Base Apparel</h4>
             <p>HTML</p>
             <p>CSS</p>
@@ -43,7 +48,7 @@ function Projects() {
             <a href='' className='code-link'>View code</a>
           </li>
           <li>
-            <img src={placeholder}></img>
+            <img src={results}></img>
             <h4>Results Summary</h4>
             <p>HTML</p>
             <p>CSS</p>
@@ -51,7 +56,7 @@ function Projects() {
             <a href='' className='code-link'>View code</a>
           </li>
           <li>
-            <img src={placeholder}></img>
+            <img src={faq}></img>
             <h4>FAQ Accordion</h4>
             <p>HTML</p>
             <p>CSS</p>
